acfed30e5afce7f144303b7562b6c177fcbae89d
Hitesh <hiteshranaut92@gmail.com>
2021-04-03 09:05:41 +0530

    key removed

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses' ,
     headers: {
-        Authorization: 'Bearer bIo6WLYhatOIZY-aem0ANO2AfzzYG74MrQnzKPxsBea9nbAM025NdqDOF6pi2U2PVs6VSSobYdvG5cJfLZNgldYj79RBiQhVkRduINqhcsRXgQaxT897u-AY6BVnYHYx'
+        Authorization: 'Bearer '
     }
 });
